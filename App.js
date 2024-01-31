f8e08826aa2a032b55f90800a7a4cabcdeb4e28d
huanghanzhilian <1319639755@qq.com>
2024-01-31 23:47:53 +0800

    chore: start application

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>hello world!</Text>
       <StatusBar style="auto" />
     </View>
   );
