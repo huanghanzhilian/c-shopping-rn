bb77d58bbecfd591878e4d0ba23bc5b4f7686869
huanghanzhilian <1319639755@qq.com>
2024-02-14 11:29:53 +0800

    test install and use husky

---
+++ commitlint.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      ['feat', 'fix', 'docs', 'style', 'refactor', 'perf', 'test', 'chore', 'revert', 'build'],
+    ],
+    'subject-case': [0],
+  },
+}
