19632a854c2f23ecd2a3026c78e7772b091c2153
huanghanzhilian <1319639755@qq.com>
2024-02-14 12:09:14 +0800

    chore: use Lint-staged test 7

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,6 +1,6 @@
 module.exports = {
   // Lint then format TypeScript and JavaScript files
-  '/**/*.(ts|tsx|js)': filenames => [
+  '/**/*.{js,ts,tsx}': filenames => [
     `eslint --fix ${filenames.join(' ')}`,
     `prettier --write ${filenames.join(' ')}`,
   ],
