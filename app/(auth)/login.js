f4856a637089734d6ceadd71d72580e9ad5b9fbe
huanghanzhilian <1319639755@qq.com>
2024-02-14 12:14:13 +0800

    chore: use Lint-staged test 7

--- app/(auth)/login.js
+++ app/(auth)/login.js
@@ -4,19 +4,6 @@ import { Pressable, Text, View } from 'react-native'
 import { useAppDispatch, useAppSelector } from '@/hooks'
 import { setTokenAsync } from '@/store'
 
-
-
-
-
-
-
-
-
-
-
-
-
-
 export default function LoginScreen() {
   //? Assets
   const dispatch = useAppDispatch()
