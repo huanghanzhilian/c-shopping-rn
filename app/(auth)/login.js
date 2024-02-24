8a96b19bc256aa02b345f53fc0553dec0a7e89f0
huanghanzhilian <1319639755@qq.com>
2024-02-24 23:54:27 +0800

    feat: add react-hook-form

--- app/(auth)/login.js
+++ app/(auth)/login.js
@@ -1,5 +1,7 @@
 import { useRouter } from 'expo-router'
-import { Pressable, Text, View } from 'react-native'
+import { useState } from 'react'
+import { useForm, Controller } from 'react-hook-form'
+import { Button, Pressable, StyleSheet, Text, TextInput, View } from 'react-native'
 
 import { TextField } from '@/components'
 import { useAppDispatch, useAppSelector } from '@/hooks'
@@ -9,11 +11,23 @@ export default function LoginScreen() {
   //? Assets
   const dispatch = useAppDispatch()
   const router = useRouter()
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm()
+  const [submittedData, setSubmittedData] = useState(null)
 
   //? Store
   const { status } = useAppSelector(state => state.user)
 
   //? Handlers
+  const onSubmit = data => {
+    // Simulate form submission
+    console.log('Submitted Data:', data)
+    setSubmittedData(data)
+  }
+
   const onLogIn = async () => {
     try {
       await dispatch(
@@ -27,6 +41,38 @@ export default function LoginScreen() {
 
   return (
     <View className="flex-1 w-[100vw] items-center justify-center bg-white">
+      <Controller
+        control={control}
+        render={({ field }) => (
+          <TextInput {...field} style={styles.input} placeholder="Your Name" />
+        )}
+        name="name"
+        rules={{ required: 'You must enter your name' }}
+      />
+      {errors.name && <Text style={styles.errorText}>{errors.name.message}</Text>}
+
+      <Controller
+        control={control}
+        render={({ field }) => <TextInput {...field} style={styles.input} placeholder="Email" />}
+        name="email"
+        rules={{
+          required: 'You must enter your email',
+          pattern: { value: /^\S+@\S+$/i, message: 'Enter a valid email address' },
+        }}
+      />
+      {errors.email && <Text style={styles.errorText}>{errors.email.message}</Text>}
+
+      {/* Submit Butonu */}
+      <Button title="Submit" onPress={handleSubmit(onSubmit)} />
+
+      {/* Gönderilen Veriler */}
+      {submittedData && (
+        <View style={styles.submittedContainer}>
+          <Text style={styles.submittedTitle}>Submitted Data:</Text>
+          <Text>Name: {submittedData.name}</Text>
+          <Text>Email: {submittedData.email}</Text>
+        </View>
+      )}
       <TextField />
       <Pressable onPress={onLogIn}>
         <Text>Login: {status}</Text>
@@ -34,3 +80,20 @@ export default function LoginScreen() {
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 16,
+  },
+  input: {
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    marginBottom: 10,
+    padding: 8,
+  },
+  errorText: {
+    color: 'red',
+    marginBottom: 10,
+  },
+})
